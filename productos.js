331ec8c602d1fbcef9543be15dbed13305c4ab44
Nicolás Vega Cardozo <nicolasricardovegacardozo@gmail.com>
2023-03-26 23:12:30 -0300

    first commit

---
+++ productos.js
@@ -0,0 +1,66 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Cartuchera",
+        precio: 500,
+        img:
+            "img/cartuchera.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Lapices de colores",
+        precio: 1000,
+        img:
+            "img/lapices-de-colores.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Voligoma",
+        precio: 250,
+        img:
+            "img/voligoma.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Cuaderno",
+        precio: 300,
+        img:
+            "img/cuaderno.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "Repuesto de hojas Nº3",
+        precio: 400,
+        img:
+            "img/repuesto-de-hojas.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 6,
+        nombre: "Mochila",
+        precio: 5000,
+        img:
+            "img/mochila.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 7,
+        nombre: "Marcadores",
+        precio: 600,
+        img:
+            "img/marcadores.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 8,
+        nombre: "Birome",
+        precio: 100,
+        img:
+            "img/birome.jpg",
+        cantidad: 1,
+    },
+]
